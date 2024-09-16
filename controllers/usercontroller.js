ea488d60fa5153926013d2083b8ac0ce2b1b6f9a
kashish taneja <kashishtaneja123@gmail.com>
2024-09-16 10:03:45 +0530

    all features updated

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,4 +1,5 @@
 const users = require('../users.json');
+const fs = require('fs');
 function getAllusers(req,res){
     try {
         res.json(users)
@@ -17,7 +18,66 @@ function getUser(req,res){
         console.log(err)
     }
 }
+function addUser(req,res){
+    try{
+        req.body.id = users.length + 1;
+        users.push(req.body);
+        fs.writeFile('users.json', JSON.stringify(users),(err)=>{
+            if(err){
+                console.log(err)
+            }
+            else{
+                res.end("user added .....")
+            }
+        })
+
+    }catch(err){
+
+    }
+}
+function editUser(req,res){
+    try{
+        let id = parseInt(req.params.id);
+        let index = users.findIndex((user)=> user.id === id)
+         console.log(index);
+         users[index].first_name = 'neha';
+
+        fs.writeFile('users.json', JSON.stringify(users),(err)=>{
+            if(err){
+                console.log(err)
+            }
+            else{
+                res.end("user edit .....")
+            }
+        })
+
+    }catch(err){
+
+    }
+}
+function deleteUser(req,res){
+    try{
+        let id = parseInt(req.params.id);
+        let index = users.findIndex((user)=> user.id === id)
+         console.log(index);
+         users.splice(index,1)
+        fs.writeFile('users.json', JSON.stringify(users),(err)=>{
+            if(err){
+                console.log(err)
+            }
+            else{
+                res.end("user delete .....")
+            }
+        })
+
+    }catch(err){
+
+    }
+}
 module.exports = {
     getAllusers,
-    getUser
+    getUser,
+    addUser,
+    editUser,
+    deleteUser
 }
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+router.use(express.json()) 
 const usercontroller = require('../controllers/usercontroller');
 router.get('/users',(req,res)=>{
     //res.json(users)
@@ -8,5 +9,13 @@ router.get('/users',(req,res)=>{
 router.get('/user/:id',(req,res)=>{
     
     usercontroller.getUser(req,res);
+})
+router.post('/add/user',(req,res)=>{
+    
+    usercontroller.addUser(req,res);
+})
+router.delete('/delete/user/:id',(req,res)=>{
+    
+    usercontroller.deleteUser(req,res);
 })
 module.exports = router;
